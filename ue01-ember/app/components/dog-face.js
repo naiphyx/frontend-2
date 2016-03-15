4a8e3a4c6af75a3e6e9caab010675066d3bdd43c
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2016-03-15 13:50:03 +0100

    add function to ue01 to show dog smile

--- ue01-ember/app/components/dog-face.js
+++ ue01-ember/app/components/dog-face.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+
+  actions: {
+    dogShowFace: function() {
+      this.$('.dogimg').css('display', 'inline')
+    }
+  }
 });
